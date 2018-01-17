f939879b714c0cb41554d37dc163cfba08ad118d
Abhishek Velayudham <avelay@terpmail.umd.edu>
2018-01-16 22:36:48 -0500

    finished coverting to Views and similifying views

--- trydjango111/src/restaurants/views.py
+++ trydjango111/src/restaurants/views.py
@@ -2,26 +2,21 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
-# function based view
-def home(request):
-    num = random.randint(0, 10000000)
-    some_list = [num, random.randint(0, 1000000), random.randint(0, 10000000)]
-    context = {
-    "bool_item": True,
-    "html_var":"context variable",
-    "num":num,
-    "some_list":some_list
-    }
-    return render(request, "home.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
-
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
+    # overrided
+    def get_context_data(self, *args, **kwargs):
+        # super(HomeView, self) basically gives the TemplateView
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = random.randint(0, 10000000)
+        some_list = [num, random.randint(0, 1000000), random.randint(0, 10000000)]
+        context.update({
+        "bool_item": True,
+        "html_var":"context variable",
+        "num":num,
+        "some_list":some_list
+        })
+        return context
